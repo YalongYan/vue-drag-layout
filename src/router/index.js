582355eea487fa71e274d7ac970dd75872d2fb53
UF201601374\Administrator <1240277991@qq.com>
2018-11-21 14:59:34 +0800

    init new branch onlyUseNativeJs 这个分支用原生js重写的

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import App from '@/App'
 import dragDemo from '@/view/dragDemo'
-import vueSlicksortPractice from '@/view/vueSlicksortPractice'
-import testDemo from '@/view/testDemo'
-import canBack from '@/view/canBack'
+// import vueSlicksortPractice from '@/view/vueSlicksortPractice'
+// import testDemo from '@/view/testDemo'
+// import canBack from '@/view/canBack'
 
 Vue.use(Router)
 
@@ -24,21 +24,21 @@ export default new Router({
       path: '/dragDemo',
       name: 'dragDemo',
       component: dragDemo
-    },
-    {
-      path: '/canBack',
-      name: 'canBack',
-      component: canBack
-    },
-    {
-      path: '/vueSlicksortPractice',
-      name: 'vueSlicksortPractice',
-      component: vueSlicksortPractice
-    },
-    {
-      path: '/testDemo',
-      name: 'testDemo',
-      component: testDemo
     }
+    // {
+    //   path: '/canBack',
+    //   name: 'canBack',
+    //   component: canBack
+    // },
+    // {
+    //   path: '/vueSlicksortPractice',
+    //   name: 'vueSlicksortPractice',
+    //   component: vueSlicksortPractice
+    // },
+    // {
+    //   path: '/testDemo',
+    //   name: 'testDemo',
+    //   component: testDemo
+    // }
   ]
 })
